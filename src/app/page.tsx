446572cf61567ff0bdb99fca6a11425c6323c39a
yundimin <ge72946@gmail.com>
2025-11-06 21:08:34 +0900

    Creating a Post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
-import ModeToggle from "@/components/ModeToggle";
-import Image from "next/image";
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
-  return <ModeToggle></ModeToggle>;
+export default async function Home() {
+  const user = await currentUser();
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20"></div>
+    </div>
+  );
 }
